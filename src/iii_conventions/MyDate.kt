da9635db99078f50ecf63c638b1c922af6c8770e
Manuel Tiago Pereira <mt.pereira@gmail.com>
2019-02-12 21:21:59 +0000

    Solve n25Comparison.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -11,3 +11,10 @@ enum class TimeInterval {
 }
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+operator fun MyDate.compareTo(date: MyDate): Int {
+    val jdn = { y: Int, m: Int, d: Int ->
+        1461 * (y + 4800 + (m - 14) / 12) / 4 + (367 * (m - 2 - 12 * ((m - 14) / 12))) / 12 - (3 * ((y + 4900 + (m - 14) / 12) / 100)) / 4 + d - 32075
+    }
+    return jdn(year, month, dayOfMonth) - jdn(date.year, date.month, date.dayOfMonth)
+}
